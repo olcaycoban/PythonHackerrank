34c1ce07fea84e4ea9fcb0b5065d2faeee867e53
Olcay Çoban <50139250+olcaycoban@users.noreply.github.com>
2020-11-17 17:23:38 +0300

    Update refinditer.py

--- refinditer.py
+++ refinditer.py
@@ -2,13 +2,14 @@ import re
 
 def solve(expressions,pattern):
     matches = list(re.finditer(r'(?={})'.format(pattern), expressions))    
-    return matches
-
+    if matches:
+        for i in matches:
+            print((i.start(),i.end()+len(pattern)-1))
+    else:
+        print((-1,-1))  
 def main():
     express = input()
     patrs=input()
-    a=solve(express,patrs)
-    for i in a:
-        print((i.start(),i.end()+len(patrs)-1))
+    solve(express,patrs) 
 if __name__=='__main__':
     main()
